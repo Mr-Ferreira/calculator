4db58dcd8710c3e6bcbe7b4bb9cd5310b642a12e
Matheus Ferreira <mathew.r.ferreira@gmail.com>
2023-12-21 14:48:34 -0500

    0.0.4.3

--- CalcScript.js
+++ CalcScript.js
@@ -443,10 +443,10 @@ function appendHistory (string) {
 // Animates buttons when clicked
 function buttonClick(event) {
     let color = window.getComputedStyle(event.srcElement , null).getPropertyValue("background-color")
-    event.srcElement.style.backgroundColor = "rgb(143, 143, 143)"
-    let changeColorBack = setInterval(function () {
-        event.srcElement.style.backgroundColor = color
-    }, 150)
+    if (color != "rgb(143, 143, 143)") {
+        event.srcElement.style.backgroundColor = "rgb(143, 143, 143)"
+        setTimeout(() => {event.srcElement.style.backgroundColor = color}, 150)
+    }
 }
 
 // Parses the formula to be later solved.
